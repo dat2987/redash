f7dca8a990db0bfcac586955408347b851507892
dat2987 <dat2987@gmail.com>
2022-03-05 14:43:33 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -50,8 +50,7 @@ create_config() {
 
 setup_compose() {
   cd "$REDASH_BASE_PATH"
-  GIT_BRANCH="${REDASH_BRANCH:-master}" # Default branch/version to master if not specified in REDASH_BRANCH env var
-  curl -OL https://raw.githubusercontent.com/dat2987/redash/"$GIT_BRANCH"/data/docker-compose.yml
+  curl -OL https://raw.githubusercontent.com/dat2987/redash/main/docker-compose.yml
   echo "export COMPOSE_PROJECT_NAME=redash" >>~/.profile
   echo "export COMPOSE_FILE=/$REDASH_BASE_PATH/docker-compose.yml" >>~/.profile
   export COMPOSE_PROJECT_NAME=redash
